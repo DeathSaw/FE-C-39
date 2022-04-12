d360c3b3167896a95be26db103ae46f3bb235517
DeathSaw <103025001+DeathSaw@users.noreply.github.com>
2022-04-12 11:55:38 +0530

    Create P3-Functions.c

---
+++ P3-Functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void add()
+{
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    printf("Sum of a and b is %d\n",c);
+}
+void main()
+{
+    add();
+}
+
+/****************output
+Sum of a and b is 5 ***************/
