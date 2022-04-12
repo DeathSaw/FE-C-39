745a165171783108fcaf639889e04e56f48e5c06
DeathSaw <103025001+DeathSaw@users.noreply.github.com>
2022-04-12 11:53:33 +0530

    Create Function.c

---
+++ Function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void add()
+{
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    printf("Sum of a and b is %d\n",c);
+}
+void main()
+{
+    add();
+}
+
+/****************output
+
+Sum of a and b is 5 ***************/
