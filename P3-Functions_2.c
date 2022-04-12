39312d46fe8c6e169842c34de4f024637116a410
DeathSaw <103025001+DeathSaw@users.noreply.github.com>
2022-04-12 12:03:17 +0530

    Create P3-Functions_2.c

---
+++ P3-Functions_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int c = a + b;
+}
+int main()
+{
+    int sum; 
+    sum=add(5,10);
+    printf("Sum is %d\n",sum);
+    return 0;
+}
+
+/************OUTPUT
+Sum is 15 ******************/
