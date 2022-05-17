22c514cb0a3e665a8bcace14ccbbd24108b9df23
DeathSaw <103025001+DeathSaw@users.noreply.github.com>
2022-05-17 12:07:04 +0530

    Create P16 Strings.c

---
+++ P16 Strings.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char strname[30]="Spoken-Tutorial";
+    printf("The string is %s\n",strname);
+    return 0;
+}
+/***********************************
+Output-
+The string is Spoken-Tutorial
+***********************************/
