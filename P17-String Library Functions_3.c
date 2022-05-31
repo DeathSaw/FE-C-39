d91c2ef882b213d8d22e1cdc75d06004dfc9e098
DeathSaw <103025001+DeathSaw@users.noreply.github.com>
2022-05-31 11:44:01 +0530

    Create P17-String Library Functions_3.c

---
+++ P17-String Library Functions_3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str1[] = "Ice";
+    char str2[] = "Cream";
+    int i,j;
+    i = strcmp(str1,"Hello");
+    j = strcmp(str2,"Cream");
+    printf("%d, %d\n",i,j);
+    return 0;
+    
+}
+
+/**************************************
+OUTPUT
+1, 0
+**************************************/
