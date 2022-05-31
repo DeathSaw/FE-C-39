b9caa8eaeae221b8b24ba9d144bdec5223eea0da
DeathSaw <103025001+DeathSaw@users.noreply.github.com>
2022-05-31 11:33:30 +0530

    Create P17-String Library Functions_1.c

---
+++ P17-String Library Functions_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char arr[] = "Ashwini";
+    int len1 = strlen(arr);
+    printf("string = %s length = %d\n",arr,len1);
+    return 0;
+}
+
+
+/*****************************
+OUTPUT
+string = Ashwini length = 7
+******************************/
